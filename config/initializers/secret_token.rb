dda3b9747b7c43246182a9cfdb9729093b9ea201
Noah Shafi <noah.shafi@tapjoy.com>
2013-01-29 22:33:34 -0800

    herro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lveawake::Application.config.secret_token = '6434d0154348719a8e2a5fb2e29dbed3afce198d6de0dc31e5e1b909f63df002d6605751de97880099d7f29f04184f8611481a55ddf072b60cbd261088943153'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lveawake::Application.config.session_store :cookie_store, key: '_lveawake_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lveawake::Application.config.session_store :active_record_store
